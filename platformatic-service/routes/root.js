64a5908fdb7e3cfbaab4f15f94f7ed56445f2e99
Liran Tal <liran.tal@gmail.com>
2023-11-16 16:25:39 +0200

    feat: add a new route to test npm packages for security vulnerabilities
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- platformatic-service/routes/root.js
+++ platformatic-service/routes/root.js
@@ -1,8 +1,32 @@
 /// <reference path="../global.d.ts" />
-'use strict'
+"use strict";
+
+const config = require("dotenv").config();
+
 /** @param {import('fastify').FastifyInstance} fastify */
 module.exports = async function (fastify, opts) {
-  fastify.get('/example', async (request, reply) => {
-    return { hello: fastify.example }
-  })
-}
+  fastify.get("/example", async (request, reply) => {
+    return { hello: "world" };
+  });
+
+  fastify.post("/test/vulnerabilities", async (request, reply) => {
+    const packageName = request.query.packageName;
+    const packageVersion = request.query.packageVersion;
+
+    const snykApiToken = config.parsed.SNYK_API_TOKEN;
+    const url = `https://snyk.io/api/v1/test/npm/${packageName}/${packageVersion}`;
+
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `token ${snykApiToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return { vulnerabilities: data.issues?.vulnerabilities };
+  });
+};
